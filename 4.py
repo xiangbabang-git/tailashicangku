bc5f4941885cf2c0581cf7c5ca12229a6965d045
wrl <1114255461@qq.com>
2019-12-18 10:37:24 +0800

    add 4.py

---
+++ 4.py
@@ -0,0 +1,15 @@
+import pymysql
+import re
+
+db = pymysql.connect(host='localhost',
+                     port=3306,
+                     user='root',
+                     password='123456',
+                     database='dict',
+                     charset='utf8')
+cur = db.cursor()
+
+f = open('dict.txt')
+args_list = []
+for line in f:
+    result = re.findall(r"(\S+)\s+(.*)", line)
